ae901b35e997583e6b374a420abe987b9efb469b
Aron <aronaland4@gmail.com>
2022-10-12 12:02:25 +0300

    uue reacti rakenduse loomise õppimine

---
+++ reacti-rakendus/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<button className="nupp">Roheline nupp</button>
+			<p className="tekst">See on sinine tekst</p>
+			<img
+				src="https://i.ytimg.com/vi/ADCUUm6WevQ/maxresdefault.jpg"
+				alt="Audi"
+			/>
+		</div>
+	);
+}
+
+export default App;
