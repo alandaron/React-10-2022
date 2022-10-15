6cded6c109ba72f5a12842733b25479dde8b5366
Aron <aronaland4@gmail.com>
2022-10-16 00:56:33 +0300

    Näita kontakti vaid nupu vajutamisel

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
+
 function Kontakt() {
+	const [kontakt, n2itaKontakt] = useState("");
 	return (
 		<div>
 			<h2>Võta ühendust</h2>
 			<hr />
 			<div>Laiuse</div>
-			<div>+372 123 456 78</div>
 			<div>Vahtra 3-3</div>
+			<button onClick={() => n2itaKontakt("+372 123 456 78")}>
+				Võta ühendust
+			</button>
 			<br />
 			<div>Jõgeva</div>
-			<div>+372 123 456 78</div>
 			<div>Rohu 6</div>
+			<button onClick={() => n2itaKontakt("+372 123 456 78")}>
+				Võta ühendust
+			</button>
+			{kontakt !== "" && <div>Tema kontakt: {kontakt}</div>}
 		</div>
 	);
 }
