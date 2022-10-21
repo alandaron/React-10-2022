11aac80d2e6e3498de601e96ead73f7c01724732
Aron <aronaland4@gmail.com>
2022-10-21 19:25:50 +0300

    uudise lisamine ja uudiste näitamine

--- uudised/src/App.js
+++ uudised/src/App.js
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Avaleht from "./pages/Avaleht";
 import Kontakt from "./pages/Kontakt";
+import LisaUudis from "./pages/LisaUudis";
 import Meist from "./pages/Meist";
 import Uudised from "./pages/Uudised";
 
@@ -21,12 +22,16 @@ function App() {
 				<Link to="/meist">
 					<button>Meist</button>
 				</Link>
+				<Link to="/lisa-uudis">
+					<button>Lisa uudis</button>
+				</Link>
 			</div>
 			<Routes>
 				<Route path="" element={<Avaleht />} />
 				<Route path="uudised" element={<Uudised />} />
 				<Route path="kontakt" element={<Kontakt />} />
 				<Route path="meist" element={<Meist />} />
+				<Route path="lisa-uudis" element={<LisaUudis />} />
 			</Routes>
 		</div>
 	);
--- uudised/src/pages/Uudised.js
+++ uudised/src/pages/Uudised.js
@@ -1,8 +1,13 @@
 function Uudised() {
+	const uudised = JSON.parse(localStorage.getItem("uudised")) || [];
+
 	return (
 		<div>
-			<h2>See on uudiste leht</h2>
-			<div>Praegu pole uudiseid :(</div>
+			<h2>Värsked uudised</h2>
+			{uudised.length < 1 && (
+				<div>Ühtegi uudist hetkel pole :( Lisame varsti!</div>
+			)}
+			{uudised.length > 0 && uudised.map((uudis) => <div>{uudis}</div>)}
 		</div>
 	);
 }
