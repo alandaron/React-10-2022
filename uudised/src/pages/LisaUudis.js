5830138d85a1e936941dc2a09030c5413531969f
Aron <aronaland4@gmail.com>
2022-11-06 19:08:14 +0200

    lisa uudis

--- uudised/src/pages/LisaUudis.js
+++ uudised/src/pages/LisaUudis.js
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function LisaUudis() {
 	const uudisRef = useRef();
+	const [message, setMessage] = useState("");
 
 	const lisaUudis = () => {
 		const uudised = JSON.parse(localStorage.getItem("uudised")) || [];
@@ -9,10 +10,26 @@ function LisaUudis() {
 		localStorage.setItem("uudised", JSON.stringify(uudised));
 	};
 
+	const check = () => {
+		setMessage("");
+
+		if (
+			uudisRef.current.value.charAt(0) ===
+			uudisRef.current.value.charAt(0).toLowerCase()
+		) {
+			setMessage("Uudis peab algama suure algustähega!");
+		}
+
+		if (uudisRef.current.value.includes("  ")) {
+			setMessage("Sisestasid kaks tühikut, paranda!");
+		}
+	};
+
 	return (
 		<div>
+			<div>{message}</div>
 			<label>Uudise nimi: </label>
-			<input ref={uudisRef} type="text" />
+			<input onChange={check} ref={uudisRef} type="text" />
 			<button onClick={lisaUudis}>Lisa uudis</button>
 		</div>
 	);
